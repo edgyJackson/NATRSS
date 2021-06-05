84a2ce043e71dfa2d77c7656c54f3a4fb0c03965
Reggie Johnson WS <rljohns579@gmail.com>
2021-06-04 23:32:11 -0700

    syntax fix

--- SpillTracker/SpillTracker/Models/ExtraChemData.cs
+++ SpillTracker/SpillTracker/Models/ExtraChemData.cs
@@ -7,14 +7,8 @@
         public int CID { get; set; }
         public string  CAS { get; set; }
         public double MolecularWeight { get; set; }
-        public string MolecularWeightReference { get; set; }
         public double Density { get; set; }
-        public string DensityReference { get; set; }
         public double VaporPressure { get; set; }
-        public string VaporPressureReference { get; set; }
-        public string Message { get; set; }
-
-
 
     }
 
