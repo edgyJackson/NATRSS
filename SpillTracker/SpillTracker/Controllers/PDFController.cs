6abe7ab23188729e1952d237c9952bb5dcc5b545
Reggie Johnson WS <rljohns579@gmail.com>
2021-06-05 09:18:47 -0700

    pdf controller added

---
+++ SpillTracker/SpillTracker/Controllers/PDFController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpillTracker.Controllers
+{
+    public class PDFController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
